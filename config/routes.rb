38ef6e6a46c21fe1294ebc966f1c8bd286c666cc
nataliavega <nvegar@gmail.com>
2022-05-25 08:33:51 +0200

    full app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+
+    collection do
+      # don't take :id
+      get :top
+    end
+
+    member do
+      # take :id
+      get :chef
+    end
+  end
+  resources :reviews, only: [:destroy]
 end
+
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# GET /restaurants/42
+# GET /restaurants/top
+# GET /restaurants/42/chef
